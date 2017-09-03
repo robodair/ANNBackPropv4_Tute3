b616db7f5cab785d718ecfbc7c385d8d61f532f8
Alisdair Robertson <alisdair.w.robertson@gmail.com>
2017-09-03 11:03:44 +1000

    Add project files.

---
+++ ANNBackProp/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace ANNShell
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.nnChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.nnProgressBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.nnChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(911, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Quit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(911, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Test 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnTest1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 197);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(514, 404);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(910, 141);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Test 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnTest2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(10, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Run";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btnIris_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(266, 5);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(634, 160);
+            this.textBox2.TabIndex = 5;
+            // 
+            // nnChart
+            // 
+            this.nnChart.BorderlineColor = System.Drawing.Color.Gray;
+            this.nnChart.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea2.Name = "ChartArea1";
+            this.nnChart.ChartAreas.Add(chartArea2);
+            legend2.BorderColor = System.Drawing.Color.Black;
+            legend2.DockedToChartArea = "ChartArea1";
+            legend2.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Top;
+            legend2.IsDockedInsideChartArea = false;
+            legend2.Name = "Legend1";
+            legend2.TableStyle = System.Windows.Forms.DataVisualization.Charting.LegendTableStyle.Wide;
+            this.nnChart.Legends.Add(legend2);
+            this.nnChart.Location = new System.Drawing.Point(536, 197);
+            this.nnChart.Margin = new System.Windows.Forms.Padding(2);
+            this.nnChart.Name = "nnChart";
+            series3.BorderWidth = 2;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series3.Legend = "Legend1";
+            series3.Name = "Training";
+            series4.BorderWidth = 2;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series4.Legend = "Legend1";
+            series4.Name = "Testing";
+            this.nnChart.Series.Add(series3);
+            this.nnChart.Series.Add(series4);
+            this.nnChart.Size = new System.Drawing.Size(450, 403);
+            this.nnChart.TabIndex = 6;
+            this.nnChart.Text = "chart1";
+            title3.Alignment = System.Drawing.ContentAlignment.BottomCenter;
+            title3.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            title3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            title3.Name = "Epochs";
+            title3.Text = "Epochs";
+            title4.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Left;
+            title4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            title4.Name = "Title1";
+            title4.Text = "Accuracy %";
+            this.nnChart.Titles.Add(title3);
+            this.nnChart.Titles.Add(title4);
+            // 
+            // nnProgressBar
+            // 
+            this.nnProgressBar.Location = new System.Drawing.Point(12, 169);
+            this.nnProgressBar.Margin = new System.Windows.Forms.Padding(2);
+            this.nnProgressBar.Name = "nnProgressBar";
+            this.nnProgressBar.Size = new System.Drawing.Size(974, 22);
+            this.nnProgressBar.TabIndex = 7;
+            this.nnProgressBar.Click += new System.EventHandler(this.nnProgressBar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 612);
+            this.Controls.Add(this.nnProgressBar);
+            this.Controls.Add(this.nnChart);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Ann Test v4.0.00a";
+            ((System.ComponentModel.ISupportInitialize)(this.nnChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart nnChart;
+        private System.Windows.Forms.ProgressBar nnProgressBar;
+    }
+}
+
---
+++ ANNBackProp/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ANNShell
+{
+    public partial class Form1 : Form
+    {
+        string dir = @"D:\SC2016\SC2016ANNv3\SC2016ANNv3\ANNBackProp\ANNBackProp\DataFiles\";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class UI : UserInterface
+        {
+            public Form1 form;
+
+            public UI(Form1 formQ)
+            {
+                form = formQ;
+            }
+
+            public override void error(string s)
+            {
+                form.textBox2.Text = form.textBox2.Text + "ERROR>>" + s + "\r\n";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                MessageBox.Show("ERROR>>" + s, "Error", buttons);
+            }
+            public override void clear(string s)
+            {
+                form.textBox2.Text = "";
+            }
+            public override void warning(string s)
+            {
+                form.textBox2.Text = form.textBox2.Text + "WARNING>>" + s + "\r\n";
+            }
+            public override void note(string s)
+            {
+                form.textBox2.Text = form.textBox2.Text + "NOTE>>" + s + "\r\n";
+            }
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnTest1_Click(object sender, EventArgs e)
+        {
+            DataClass d = new DataClass();
+            d.readFromFile(dir, "test1.txt");
+            string s = d.showData();
+            textBox1.Text = s;
+            d.writeToFile(dir, "temp.txt");
+            d.normalize(0, 1);
+            string ss = d.showData();
+            textBox1.Text = textBox1.Text +"\r\n\r\n" +ss;
+            DataClass d1=null;
+            DataClass d2=null;
+            d.extractSplit(out d1, out d2, 4, new Random());
+            string s1 = d1.showData();
+            string s2 = d2.showData();
+            textBox1.Text = textBox1.Text + "\r\n\r\n" + s1;
+            textBox1.Text = textBox1.Text + "\r\n\r\n" + s2;
+
+        }
+
+        private void btnTest2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            DataClass d = new DataClass();
+            d.readFromFile(dir, "test1.txt");
+            string s = d.showData();
+            textBox1.Text = s;
+            textBox1.Text = textBox1.Text + "\r\n\r\n";
+
+            DataClass dd = d.makeExemplar(2, 3, 1);
+            string ss = dd.showData();
+            textBox1.Text = textBox1.Text + ss;
+            textBox1.Text = textBox1.Text + "\r\n\r\n";
+        }
+
+        private void btnIris_Click(object sender, EventArgs e)
+        {
+            // this is code for the run button
+            string datafile = "IrisDataOriginalNum.txt";
+            int inputs = 4;
+            int outputs = 3;
+            
+            // iris button
+            nnChart.Series["Training"].Points.Clear();
+            nnChart.Series["Testing"].Points.Clear();
+
+            textBox2.Clear(); // clear previous messages
+
+
+
+            DataClass irisRaw = new DataClass(dir, datafile, new UI(this));
+            string s = irisRaw.showDataPart(5, inputs+1, "F4", "4000 e 10");
+            textBox1.AppendText(s);
+            textBox1.AppendText("\r\n\r\n");
+
+            irisRaw.normalize(0, 1);
+            string ss = irisRaw.showDataPart(5, inputs+1, "F4", "4000 Data Normalised");
+            textBox1.AppendText(ss);
+            textBox1.AppendText("\r\n\r\n");
+
+            DataClass irisExemplar = irisRaw.makeExemplar(inputs, outputs, 1);
+            string se = irisExemplar.showDataPart(5, inputs+outputs, "F4", "4000 Exemplar Data");
+            textBox1.AppendText(se);
+            textBox1.AppendText("\r\n\r\n");
+
+            DataClass trainData = new DataClass();
+            DataClass testData = new DataClass();
+            DataClass valData = new DataClass();
+            DataClass tempData = new DataClass();
+
+            Random rnd1 = new Random(103);
+            irisExemplar.extractSplit(out trainData, out tempData, 50, rnd1);
+            tempData.extractSplit(out testData, out valData, 50, rnd1);
+            trainData.writeToFile(dir, "tempTrain.txt"); // debug
+            testData.writeToFile(dir, "tempTest.txt");
+            valData.writeToFile(dir, "tempVal.txt");
+            
+            string s1 = trainData.showDataPart(5, inputs+outputs, "F4", "Training Data");
+            textBox1.AppendText(s1);
+            textBox1.AppendText("\r\n\r\n");
+
+            string s2 = testData.showDataPart(5, inputs+outputs, "F4", "Testing Data");
+            textBox1.AppendText(s2);
+            textBox1.AppendText("\r\n\r\n");
+
+            string s3 = valData.showDataPart(5, inputs+outputs, "F4", "Validation Data");
+            textBox1.AppendText(s3);
+            textBox1.AppendText("\r\n\r\n");
+
+            NeuralNetwork nn = new NeuralNetwork(inputs, 4, outputs, new UI(this), rnd1);
+            nn.InitializeWeights(rnd1);
+            //Console.WriteLine("\nBeginning training using incremental back-propagation\n");
+            nn.train(trainData.data, testData.data, 200, 0.05, dir+"nnlog.txt", nnChart, nnProgressBar);
+            //Console.WriteLine("Training complete");
+
+            double trainAcc = nn.Accuracy(trainData.data,dir+"trainOut.txt");
+            string ConfusionTrain = nn.showConfusion(dir+"trainConfusion.txt");
+            double testAcc = nn.Accuracy(testData.data,dir+"testOut.txt");
+            string ConfusionTest = nn.showConfusion(dir + "testConfusion.txt");
+            double valAcc = nn.Accuracy(valData.data,dir+"valOut.txt");
+            string ConfusionVal = nn.showConfusion(dir + "valConfusion.txt");
+
+            // convert accuracy to percents
+            trainAcc = trainAcc * 100;
+            testAcc = testAcc * 100;
+            valAcc = valAcc * 100;
+            textBox1.AppendText("Training Accuracy   = " + trainAcc.ToString("F2") + "\r\n");
+            textBox1.AppendText("Testing Accuracy    = " + testAcc.ToString("F2") + "\r\n");
+            textBox1.AppendText("Validation Accuracy = " + valAcc.ToString("F2") + "\r\n");
+            textBox1.AppendText("\r\n\r\n");
+
+            textBox1.AppendText("Training Confusion Matrix \r\n"+ConfusionTrain+ "\r\n\r\n");
+            textBox1.AppendText("Testing Confusion Matrix \r\n"+ConfusionTest+ "\r\n\r\n");
+            textBox1.AppendText("Validation Confusion Matrix \r\n"+ConfusionVal+ "\r\n\r\n");
+        }
+
+        private void nnProgressBar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
